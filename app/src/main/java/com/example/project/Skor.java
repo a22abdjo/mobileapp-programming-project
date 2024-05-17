1a752aec4e0dd3f1a857c2ea54eb7220bb2b621d
a22abdjo <a22abdjo@student.his.se>
2024-05-17 16:57:30 +0200

    attributes: prejektet

--- app/src/main/java/com/example/project/Skor.java
+++ app/src/main/java/com/example/project/Skor.java
@@ -2,19 +2,19 @@ package com.example.project;
 
 public class Skor {
     private String name;
-    private String size;
-    private int height;
+    private int size;
+    private int cost;
 
     public Skor() {
         name = "Namn saknas";
-        size = "----";
-        height = 1;
+        size = 0;
+        cost = 0;
     }
 
-    public Skor(String n, String s, int h) {
-        this.name = n;
-        this.size = s;
-        this.height = h;
+    public Skor(String n, int s, int c) {
+        name = n;
+        size = s;
+        cost = c;
     }
 
     public Skor(String name) {
@@ -29,7 +29,17 @@ public class Skor {
         this.name = name;
     }
 
-    public String toString() {
-        return name;
+    public int getSize() {
+        return size;
     }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public String toString() {
+        return "Brand: " + name + "\n"+"Size: "+ size + "\n" +"Cost: "+ cost+ "kr";
+    }
+
+
 }
